556cd9970d9ffa5e2e217d380d0bd01f610c4831
Jacob Hands <jrhands@gmail.com>
2018-07-25 16:11:49 -0400

    Initial commit for BlueHellBot

---
+++ BlueHellBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlueHellBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
